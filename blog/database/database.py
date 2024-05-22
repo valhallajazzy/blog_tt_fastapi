96e1d137ca73eaef117a0836ef1df7428f4f42c7
valhallajazzy <valhallajazzy@mail.ru>
2024-05-22 23:31:45 +0500

    Create models

---
+++ blog/database/database.py
@@ -0,0 +1,20 @@
+import os
+from typing import AsyncGenerator
+
+from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL')
+
+engine = create_async_engine(
+    SQLALCHEMY_DATABASE_URL)
+
+async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
---
+++ blog/database/models.py
@@ -0,0 +1,67 @@
+from sqlalchemy import ForeignKey, text
+from sqlalchemy.orm import Mapped, DeclarativeBase, mapped_column, relationship
+from datetime import datetime
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Author(Base):
+    __tablename__ = "authors"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    first_name: Mapped[str] = mapped_column(nullable=False)
+    last_name: Mapped[str] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(nullable=False, unique=True)
+    posts: Mapped[list["Post"]] = relationship(
+        back_populates="author"
+    )
+
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    published_at: Mapped[datetime] = mapped_column(server_default=text("TIMEZONE('utc', now())"))
+    title: Mapped[str] = mapped_column(nullable=False)
+    text: Mapped[str] = mapped_column(nullable=False)
+    author_id: Mapped[int] = mapped_column(ForeignKey("authors.id", ondelete="CASCADE"))
+    category_id: Mapped[int] = mapped_column(ForeignKey("categories.id", ondelete="CASCADE"))
+
+    tags: Mapped[list["Tag"]] = relationship(
+        back_populates="many_posts",
+        secondary="posts_tags",
+    )
+
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    category_name: Mapped[str] = mapped_column(nullable=False, unique=True)
+
+
+class Tag(Base):
+    __tablename__ = "tags"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(nullable=False)
+
+    posts: Mapped[list["Post"]] = relationship(
+        back_populates="many_tags",
+        secondary="posts_tags",
+    )
+
+
+class PostsTags(Base):
+    __tablename__ = "posts_tags"
+
+    post_id: Mapped[int] = mapped_column(
+        ForeignKey("posts.id", ondelete="CASCADE"),
+        primary_key=True
+    )
+    tag_id: Mapped[int] = mapped_column(
+        ForeignKey("tags.id", ondelete="CASCADE"),
+        primary_key=True
+    )
